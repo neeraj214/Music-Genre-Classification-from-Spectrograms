a86a90337c0fa01ef71dad4339ef4116ce9fd0b0
Neeraj Negi <150280096+neeraj214@users.noreply.github.com>
2026-04-02 16:23:53 +0530

    feat: create HomePage component

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from '../components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <div className="w-full">
+      <HeroSection />
+    </div>
+  );
+};
+
+export default HomePage;
